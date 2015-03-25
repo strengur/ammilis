8ddecf4ba4151f24dc78792294805b733ce5750f
strengur <steingrimur@740.is>
2015-03-25 16:29:43 +0000

    Mobile menu setup
    Finished the setup of mobile menu. jquery used to slide the menu down
    and to change the menu button.

--- js/main-java.js
+++ js/main-java.js
@@ -3,3 +3,46 @@
 $("#banner-headline-image-left").delay(4000).fadeIn(1000);
 $("#banner-headline-text").delay(4300).fadeIn(1300);
 $("#banner-headline-image-right").delay(4600).fadeIn(1000);
+
+/*
+$(".navigation-mobile-menu").toggle(function() {
+	$(".navigation-mobile-menu-closed").slideDown(800);
+*/
+
+
+$(".navigation-mobile-menu").click(function() {
+	
+	$(".navigation-mobile-menu-closed").slideToggle("slow", function() {
+		
+		var openMobileMenu = "+";
+		var closeMobileMenu = "-";
+		
+		if ($(".navigation-mobile-menu").text() === openMobileMenu){
+			$(".navigation-mobile-menu").html(closeMobileMenu);
+		} else {
+			$(".navigation-mobile-menu").html(openMobileMenu);
+		}
+
+/*
+		$(".navigation-mobile-menu").html("&nbsp;&diams;&nbsp;");
+		$(".navigation-mobile-menu").html("&nbsp;&loz;&nbsp;");
+*/
+
+	});
+			
+});
+
+/*
+	$(".navigation-mobile-menu-closed").slideDown(800);	
+	$(".navigation-mobile-menu").html("&nbsp;&diams;&nbsp;");
+	$(".navigation-mobile-menu").addClass("navigation-mobile-menu-clicked");
+*/
+//});
+/*
+
+$(".button-grey-gradient").click(function() {
+
+	$(".navigation-mobile-menu-closed").html("hggh");	
+	$(".navigation-mobile-menu").html("&nbsp;&loz;&nbsp;");
+});
+*/
---
+++ navigation.php
@@ -0,0 +1,31 @@
+<header>
+	<div class="logo">
+		<img src="images/logo.png" alt="Merki fyrirtækisins.">
+	</div>
+	
+	<div class="navigation">
+		<ul>
+			<li>HEIM</li>
+			<li>GREINAR</li>
+			<li>UM OKKUR</li>
+			<li>HAFA SAMBAND</li>
+			<li><button class="navigation-online-store button-blue-gradient"><a href="http://n.likamioglifsstill.is/">NETVERSLUN</a></button></li>
+		</ul>
+	</div>
+	
+	<div class="navigation-mobile">
+		<ul>
+			<li><button class="navigation-online-store button-blue-gradient"><a href="http://n.likamioglifsstill.is/">NETVERSLUN</a></button></li>
+			<li><button class="navigation-mobile-menu button-grey-gradient"><span>+</span></li>
+		</ul>
+	</div>
+	
+	<div class="navigation-mobile-menu-closed">
+		<ul>
+			<li>HEIM</li>
+			<li>GREINAR</li>
+			<li>UM OKKUR</li>
+			<li>HAFA SAMBAND</li>
+		</ul>
+	</div>
+</header>
