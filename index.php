fbdfcc296f67b4f696a83f4c82442242c750dd2b
strengur <steingrimur@740.is>
2015-04-04 14:31:32 +0000

    Articles desktop ready
    Two columns article list

--- index.php
+++ index.php
@@ -32,9 +32,9 @@
 	
 </div>
 
-<div class="article-columns">
+<div class="article-columns article-columns-img">
 	
-	<img src="images/banners/female-running.jpg" alt="Bakgrunnsmynd þar sem nýjustu greinarnar birtast.">
+<!-- 	<img src="images/banners/female-running.jpg" alt="Bakgrunnsmynd þar sem nýjustu greinarnar birtast."> -->
 	
 	<div class="article-columns-content">
 		
@@ -42,17 +42,32 @@
 		<div class="newest-article"><h1>NÝJUSTU GREINAR</h1></div>
 		<div class="article-columns-border-bottom"></div>
 		
-		<div class="article-column-one">
-			<h1>Tileinkaður þér góðar venjur</h1>
-			<p>Tileinkaðu þér góðar venjur í mataræði og vatnsdrykkju og árangurinn kemur. Allt of margir búast við árangri strax og gefast upp að lokum eftir aðeins mánuð. Jafnvel vitandi það að fólkið sem er í fanta formi er búið að leggja á sig margra ára puð við að ná þeim árangri sem það nýtur í dag, [...]</p>
+		
+		<div class="article-columns-logo-left">
+			<img src="images/icons/left-side-logo-blue.png" alt="Vinstri hlið af LL logoi">
 		</div>
 		
-		<div class="article-column-two">
-			<h1>Ástarhandföngin burt</h1>
-			<p>Það er næstum hægt að fullyrða að engum líkar að hafa ástarhandföng (hliðarspik) sem fara yfir buxnastrenginn eða skemmir flott snið á fötum. Ástarhandföngin eru ein helsta ástæða þess að fólk æfir og eftir þúsundir endurtekninga á hinum ýmsu kviðæfingum gefst fólk upp og fær sér oft einkaþjálfara. Flest fólk heldur að með því að [...]</p>
+		<div class="article-columns-center-fix">
+			<div class="article-column-one">
+				<h1>Tileinkaður þér góðar venjur</h1>
+				<p>Tileinkaðu þér góðar venjur í mataræði og vatnsdrykkju og árangurinn kemur. Allt of margir búast við árangri strax og gefast upp að lokum eftir aðeins mánuð. Jafnvel vitandi það að fólkið sem er í fanta formi er búið að leggja á sig margra ára puð við að ná þeim árangri sem það nýtur í dag, [...]</p>
+			</div>
+			
+			<div class="article-column-two">
+				<h1>Ástarhandföngin burt</h1>
+				<p>Það er næstum hægt að fullyrða að engum líkar að hafa ástarhandföng (hliðarspik) sem fara yfir buxnastrenginn eða skemmir flott snið á fötum. Ástarhandföngin eru ein helsta ástæða þess að fólk æfir og eftir þúsundir endurtekninga á hinum ýmsu kviðæfingum gefst fólk upp og fær sér oft einkaþjálfara. Flest fólk heldur að með því að [...]</p>
+			</div>
 		</div>
 		
+		<div class="article-columns-logo-right">
+			<img src="images/icons/right-side-logo-blue.png" alt="Hægri hlið af LL logoi">
+		</div>
+	
 	</div>
+</div>
+
+<div class="ad-banners">
+	
 </div>
 
 <?php include('footer.php'); ?>
