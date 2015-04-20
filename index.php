b1fc482cf637ef7b317d78431a9603176c6c24b0
strengur <steingrimur@740.is>
2015-04-20 10:54:29 +0000

    Commit before media changes

--- index.php
+++ index.php
@@ -8,18 +8,20 @@
 			
 <!-- 			<img id="banner-headline-image-left" src="images/icons/left-side-logo-blue.png" alt="Vinstri hlið af LL logoi"> -->
 			<h1>
-				<span id="banner-headline-text">Við hjálpum þér að ná þínum markmiðum!</span>	
+				Við hjálpum þér að ná þínum markmiðum!	
 			</h1>
 <!-- 			<img id="banner-headline-image-right" src="images/icons/right-side-logo-blue.png" alt="Hægri hlið af LL logoi"> -->
 	</div>
 	
-	<div class="banner-button-clearfix">
+	
+
+</div>
+
+<div class="banner-button-clearfix">
 		<div class="banner-button">
 			<a href="#" class="banner-button-button button-blue-gradient button">Nánar...</a>
 		</div>
 	</div>
-
-</div>
 
 <div class="arrow-down-clearfix">
 	<div class="arrow-down">
--- js/main-java.js
+++ js/main-java.js
@@ -1,8 +1,8 @@
 //Show LL images and the text in between
 
-$("#banner-headline-image-left").delay(4000).fadeIn(1000);
-$("#banner-headline-text").delay(4300).fadeIn(1300);
-$("#banner-headline-image-right").delay(4600).fadeIn(1000);
+// $("#banner-headline-image-left").delay(3000).fadeIn(1000);
+$(".banner-headline").delay(2300).fadeIn(1300);
+// $("#banner-headline-image-right").delay(4600).fadeIn(1000);
 
 
 // Mobile version of main menu, dropdown menu
